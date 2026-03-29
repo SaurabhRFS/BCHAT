944f95bff9d69eb9aa51fc1c68c43101998f41ff
SaurabhRFS <saurabhrfs2@gmail.com>
2026-03-29 11:37:37 +0530

    final done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
